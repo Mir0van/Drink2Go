b03a82283da9eab6c0ef17efaf1758a401d9f66f
Vladimir <vokar90@yandex.ru>
2023-02-21 12:42:58 +0300

    add: header styles and js

---
+++ source/js/nav-toogle.js
@@ -0,0 +1,15 @@
+let mainNav = document.querySelector('.main-nav');
+let mainNavToggle = document.querySelector('.main-nav__toogle');
+let userNav = document.querySelector('.main-nav__user-nav');
+let primaryNav = document.querySelector('.main-nav__primary-nav');
+
+mainNavToggle.classList.remove('main-nav__toogle--no-js');
+userNav.classList.add('main-nav__user-nav--js-menu');
+primaryNav.classList.add('main-nav__primary-nav--menu-closed');
+
+mainNavToggle.addEventListener('click', () => {
+  mainNavToggle.classList.toggle('main-nav__toogle--closed')
+  mainNavToggle.classList.toggle('main-nav__toogle--opened')
+  primaryNav.classList.toggle('main-nav__primary-nav--menu-closed')
+  primaryNav.classList.toggle('main-nav__primary-nav--menu-opened')
+});
