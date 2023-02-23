bffc775ae762e68b2e1e80b90b48673964a5a37b
Vladimir <vokar90@yandex.ru>
2023-02-23 16:21:30 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,1 @@
+import"./nav-toogle.js";import"./swiper.js";import"./select-toogle.js";import"./map.js";
---
+++ js/swiper.js
@@ -0,0 +1,1 @@
+const initSwiper=()=>{new Swiper(".swiper",{initialSlide:0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})};new Swiper(".swiper",{initialSlide:0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
