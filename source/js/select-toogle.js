03c2c1879ba34230a9ad7552c417358ba87472c5
Vladimir <vokar90@yandex.ru>
2023-02-24 15:50:49 +0300

    add: custom select

--- source/js/select-toogle.js
+++ source/js/select-toogle.js
@@ -1,9 +1,21 @@
-let sortSelect = document.querySelector('.catalog__select');
+let sortSelect = document.querySelector('.select__button');
+const textButton = document.querySelector('.select__text-button');
+const options = document.querySelectorAll('.select__option');
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
+const optionClickHanler = (option) => {
+  let selectedOptions = option.querySelector('.select__option-text');
+  textButton.innerText = selectedOptions.innerText;
+
+  options.forEach((option) => {
+    option.querySelector('.select__option-text').classList.remove('select__option-text--selected');
+  });
+  selectedOptions.classList.add('select__option-text--selected');
+};
+
 const scrollHandler = () => {
-  sortSelect.classList.remove('catalog__select--opened');
-  sortSelect.classList.add('catalog__select--closed');
+  sortSelect.classList.remove('select__button--opened');
+  sortSelect.classList.add('select__button--closed');
 
   window.removeEventListener('scroll', scrollHandler);
   document.removeEventListener('click', clickHandler);
@@ -17,19 +29,23 @@ function selectorEscKeydownHandler (evt) {
 }
 
 function clickHandler (evt) {
-  if (!evt.target.closest('.catalog__select')) {
+  if (!evt.target.closest('.select__button')) {
     scrollHandler();
   }
 }
 
 const toogleSortSelect = () => {
   sortSelect.addEventListener('click', () => {
-    sortSelect.classList.toggle('catalog__select--closed')
-    sortSelect.classList.toggle('catalog__select--opened')
+    sortSelect.classList.toggle('select__button--closed')
+    sortSelect.classList.toggle('select__button--opened')
 
     document.addEventListener('click', clickHandler);
     document.addEventListener('keydown', selectorEscKeydownHandler);
     window.addEventListener('scroll', scrollHandler);
+  });
+
+  options.forEach((option) => {
+    option.addEventListener('click', optionClickHanler.bind(this, option));
   });
 }
 
